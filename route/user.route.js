b0bbff4c5b3fe3b285be5cef66de7aadcf796759
Adaji Mukhtar <alfadaji@gmail.com>
2025-12-19 06:20:28 +0100

    added the sign in method

--- route/user.route.js
+++ route/user.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/users.controller.js";
 
 const userRouter = Router();
 // Define your user routes here
 
 userRouter.post("/", (req, res) => res.send({ title: "CREATE a user" }));
-userRouter.get("/", (req, res) => res.send({ title: "GETs all users" }));
-userRouter.get("/:id", (req, res) => res.send({ title: "GET a user" }));
+userRouter.get("/", getUsers);
+userRouter.get("/:id", getUser);
 userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE a user" }));
 userRouter.delete("/:id", (req, res) => res.send({ title: "DELETE a user" }));
 
